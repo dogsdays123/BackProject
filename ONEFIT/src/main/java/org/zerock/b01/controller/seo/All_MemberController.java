4c2a00a782371fecdfed959cc7b717f86c1ff881
dogsdays123 <97099336+dogsdays123@users.noreply.github.com>
2025-03-14 15:09:42 +0900

    c
    commit

--- ONEFIT/src/main/java/org/zerock/b01/controller/seo/All_MemberController.java
+++ ONEFIT/src/main/java/org/zerock/b01/controller/seo/All_MemberController.java
@@ -47,7 +47,7 @@ public class All_MemberController {
 
     @GetMapping("/login")
     public void login() {
-        log.info("login dawd");
+        log.info("login");
     }
 
     @GetMapping("/join")
