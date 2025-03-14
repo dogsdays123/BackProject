e83328e52e618e3c4cb8c36d11f5ab4a2cee4ddf
725126 <sjw725126@gmail.com>
2025-03-14 16:07:13 +0900

    신지원
    상품, 기구, 시설 테이블
    수정했습니다.

--- ONEFIT/src/main/java/org/zerock/b01/domain/transaction/Equipment.java
+++ ONEFIT/src/main/java/org/zerock/b01/domain/transaction/Equipment.java
@@ -10,24 +10,33 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Equipment {
+public class Equipment { // (거래 - 상품) 기구
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long equipment_id;
+    private Long equipment_id; // 기구 ID
 
     @Column(length = 255, nullable = false)
-    private String p_pur_price;
+    private String e_name; // 1. 제품명
 
-    @Column(nullable = false)
-    private LocalDate p_use_start;
-
-    @Column(nullable = false)
-    private LocalDate p_use_end;
+    @Column(length = 255, nullable = false)
+    private String e_brand; // 2. 제조사
 
     @Column(length = 20, nullable = false)
-    private String p_as;
+    private String e_status; // 3. 제품 상태(상, 중, 하)
+
+    @Column(length = 255, nullable = false)
+    private String e_pur_price; // 4. 구매가
+
+    @Column(nullable = false)
+    private LocalDate e_use_start; // 5. 사용 시작일
+
+    @Column(nullable = false)
+    private LocalDate e_use_end; // 6. 사용 종료일
+
+    @Column(length = 20, nullable = false)
+    private String e_as; // 7. A/S 가능여부
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
+    private Product product; // (외래키) 상품 ID
 }
--- ONEFIT/src/main/java/org/zerock/b01/domain/transaction/Facility.java
+++ ONEFIT/src/main/java/org/zerock/b01/domain/transaction/Facility.java
@@ -10,33 +10,33 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class Facility {
+public class Facility { // (거래 - 상품) 시설
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long facility_id;
+    private Long facility_id; // 시설 ID
 
     @Column(length = 255, nullable = false)
-    private String p_center_name;
+    private String f_center_name; // 1. 센터명
 
     @Column(nullable = false, precision = 10, scale = 0)
-    private BigDecimal p_deposit;
+    private BigDecimal f_deposit; // 2. 보증금
 
     @Column(nullable = false, precision = 10, scale = 0)
-    private BigDecimal p_month_rent;
+    private BigDecimal f_month_rent; // 3. 월세
 
     @Column(nullable = false, precision = 10, scale = 0)
-    private BigDecimal p_admin_cost;
+    private BigDecimal f_admin_cost; // 4. 관리비
 
     @Column(length = 255, nullable = false)
-    private String p_reason_sale;
+    private String f_reason_sale; // 5. 매매사유
 
     @Column(nullable = false, precision = 7, scale = 2)
-    private BigDecimal p_cont_area;
+    private BigDecimal f_cont_area; // 6. 계약 면적
 
     @Column(nullable = false, precision = 7, scale = 2)
-    private BigDecimal p_real_area;
+    private BigDecimal f_real_area; // 7. 실 면적
 
     @ManyToOne
     @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
+    private Product product; // (외래키) 상품 ID
 }
