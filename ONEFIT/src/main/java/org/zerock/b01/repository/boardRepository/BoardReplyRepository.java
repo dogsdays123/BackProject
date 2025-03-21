a7a7030834284bddbe94f5b99c27285a1479747c
ohongo <dnjswls53@gmail.com>
2025-03-21 16:27:02 +0900

    ㄴㄴ
    ㄴㄴ

--- ONEFIT/src/main/java/org/zerock/b01/repository/boardRepository/BoardReplyRepository.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.b01.repository.boardRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.zerock.b01.domain.board.Board_Reply;
-
-public interface BoardReplyRepository extends JpaRepository<Board_Reply, Long> {
-}
--- ONEFIT/src/main/resources/static/js/trainer_view.js
+++
@@ -1,2 +0,0 @@
-tableTimeCalc();
-thumbnails.dispatchEvent(new Event('change'));
