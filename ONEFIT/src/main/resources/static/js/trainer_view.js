e68bfb9d92ae2c63185711bd2f1f1a1e7048bbe3
soleStudent <bangsj0616@naver.com>
2025-03-21 12:16:51 +0900

    스크립트 작업
    제곧네

--- ONEFIT/src/main/resources/static/js/trainer_view.js
+++ ONEFIT/src/main/resources/static/js/trainer_view.js
@@ -1,2 +1,40 @@
 tableTimeCalc();
 thumbnails.dispatchEvent(new Event('change'));
+
+const academyContent = JSON.parse(viewDTO.academy);
+const careerContent = JSON.parse(viewDTO.career);
+const licenseContent = JSON.parse(viewDTO.license);
+const prizeContent = JSON.parse(viewDTO.prize);
+const academyTable = document.getElementById('academy-table-body');
+const careerTable = document.getElementById("career-table-body");
+const licenseTable = document.getElementById("license-table-body");
+const prizeTable = document.getElementById("prize-table-body");
+
+function createTableRow(value) {
+    const tr = document.createElement("tr");
+    const td = document.createElement("td");
+    const school = document.createElement("input");
+    school.type = "text";
+    school.classList.add("form-control", "do-not-submit");
+    school.readOnly = true;
+    tr.appendChild(td);
+    td.appendChild(school);
+    school.value = value;
+    return tr;
+}
+
+for (const academy of academyContent) {
+
+}
+
+for (const career of careerContent) {
+
+}
+
+for (const license of licenseContent) {
+
+}
+
+for (const prize of prizeContent) {
+
+}
