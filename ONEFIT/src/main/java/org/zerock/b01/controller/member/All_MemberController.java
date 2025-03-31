0cb038446b1916b3e84de4295956d67869cd3f38
dogsdays123 <97099336+dogsdays123@users.noreply.github.com>
2025-03-31 11:49:17 +0900

    cc
    cc

--- ONEFIT/src/main/java/org/zerock/b01/controller/member/All_MemberController.java
+++ ONEFIT/src/main/java/org/zerock/b01/controller/member/All_MemberController.java
@@ -5,6 +5,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -25,12 +30,16 @@ import org.zerock.b01.dto.memberDTO.*;
 import org.zerock.b01.dto.recruitDTO.RecruitDTO;
 import org.zerock.b01.dto.trainerDTO.TrainerDTO;
 import org.zerock.b01.dto.trainerDTO.TrainerViewDTO;
+import org.zerock.b01.dto.trainerDTO.Trainer_ThumbnailsDTO;
 import org.zerock.b01.security.dto.MemberSecurityDTO;
 import org.zerock.b01.service.All_MemberService;
 import org.zerock.b01.service.memberService.Member_Set_Type_Service;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -40,6 +49,9 @@ import java.util.Map;
 @Log4j2
 @RequiredArgsConstructor
 public class All_MemberController {
+
+    @Value("${org.zerock.upload.path}")
+    private String thumbnailPath;
 
     private final All_MemberService all_memberService;
     private final Member_Set_Type_Service member_Set_Type_Service;
@@ -148,25 +160,7 @@ public class All_MemberController {
       
         if(all_memberDTO !=null) {
             AllBoardSearchDTO allBoard = all_memberService.boardReadForAllMember(all_memberDTO.getAllId());
-            if(allBoard == null) {
-                model.addAttribute("allBoardDTO", null);
-            } else {
-                model.addAttribute("allBoardDTO", allBoard);
-                if(allBoard.getNoticeBoardDTO().isEmpty() && allBoard.getQnaBoardDTO().isEmpty()) {
-                    model.addAttribute("noticeBoardDTO", allBoard.getNoticeBoardDTO());
-                    model.addAttribute("qnaBoardDTO", allBoard.getQnaBoardDTO());
-                } else if (allBoard.getNoticeBoardDTO().isEmpty()) {
-                    model.addAttribute("noticeBoardDTO", null);
-                    model.addAttribute("qnaBoardDTO", allBoard.getQnaBoardDTO());
-                } else if (allBoard.getQnaBoardDTO().isEmpty()){
-                    model.addAttribute("noticeBoardDTO", allBoard.getNoticeBoardDTO());
-                    model.addAttribute("qnaBoardDTO", null);
-                } else {
-                    model.addAttribute("noticeBoardDTO", null);
-                    model.addAttribute("qnaBoardDTO", null);
-                }
-            }
-
+            model.addAttribute("allBoardDTO", allBoard);
             log.info("board####" + allBoard);
         }
     }
@@ -187,6 +181,7 @@ public class All_MemberController {
 
         //이력서 찾는 코드
         TrainerDTO trainerDTO = member_Set_Type_Service.trainerReadForUser(user_memberDTO.getUserId());
+
         if(trainerDTO != null) {
             log.info("%%%%" + trainerDTO);
 
@@ -200,11 +195,36 @@ public class All_MemberController {
             } catch (Exception e) {
                 log.error("JSON 변환 오류", e);
             }
+
+            //이력서 섬네일
+            String[] thumbnail = member_Set_Type_Service.searchThumbnail(trainerDTO.getTrainerId());
+            model.addAttribute("thumbnailUuid", thumbnail[0]);
+            model.addAttribute("thumbnailName", thumbnail[1]);
+            //-------------
+
         } else{
             trainerDTO = null;
         }
+
         log.info("$#$#$#" + trainerDTO);
         model.addAttribute("trainerDTO", trainerDTO);
+    }
+
+    //섬네일 불러오기 전용
+    @GetMapping("/my_user_view/{path}")
+    public ResponseEntity<Resource> my_user_viewGET(@PathVariable String path) {
+
+        //이력서 섬네일
+        Resource resource = new FileSystemResource(thumbnailPath + File.separator + path);
+        HttpHeaders headers = new HttpHeaders();
+
+        try{
+            headers.add("Content-Type", Files.probeContentType(resource.getFile().toPath()));
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+        return ResponseEntity.ok().headers(headers).body(resource);
+        //-----------
     }
 
     @PreAuthorize("isAuthenticated()")
