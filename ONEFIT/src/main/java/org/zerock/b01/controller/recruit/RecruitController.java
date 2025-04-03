81f8ca601b533a9da97ada9b2b7a98bd53fadf30
sihyunpark <sihyun22>
2025-04-03 14:20:29 +0900

    cd
    cd

--- ONEFIT/src/main/java/org/zerock/b01/controller/recruit/RecruitController.java
+++ ONEFIT/src/main/java/org/zerock/b01/controller/recruit/RecruitController.java
@@ -207,7 +207,7 @@ public class RecruitController {
             log.info("user_memberDTO is null, redirecting to login page or error page");
 
             String script = "<script>" +
-                    "alert('개인회원만 접근할 수 있습니다.');" +
+                    "alert('로그인이 필요한 서비스입니다.');" +
                     "location.href='/recruit/read?recruitId=" + recruitId + "&" + link + "';" +
                     "</script>";
             response.setContentType("text/html; charset=UTF-8");
